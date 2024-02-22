5abcf84ee910d3f53e07e6bbccaf17adde43f122
felkan <felixg.nti@gmail.com>
2024-02-23 00:21:13 +0100

    usch

--- titelspoket/src/App.svelte
+++ titelspoket/src/App.svelte
@@ -1,48 +1,5 @@
 <script>
-  import Aix from "./routes/aix.svelte";
   import Vrak from "./routes/vrak.svelte";
-
-  let selectedRoute = 'vrak';
-
 </script>
 
-<nav>
-  <button id="vrak" on:click={() => selectedRoute = 'vrak'}>🚽</button>
-  <button id="aix" on:click={() => selectedRoute = 'aix'}>🐀</button>
-</nav>
-
-{#if selectedRoute === 'vrak'}
-  <Vrak />
-{:else if selectedRoute === 'aix'}
-  <Aix />
-{/if}
-
-<style>
-  nav {
-    display: flex;
-    justify-content: center;
-    margin-bottom: 2rem;
-  }
-
-  button {
-    border-radius: 1rem;
-    padding: 0.5rem 1rem;
-    margin: 0 0.5rem;
-    cursor: pointer;
-  }
-
-  #vrak {
-    background-color: #3f9f52;
-    border: 1px solid white;
-  }
-
-  #aix {
-    background-color: #000000;
-    border: 1px solid yellow;
-  }
-
-  button:hover {
-    background-color: #555a64;
-    color: #fff;
-  }
-</style>
+<Vrak />
